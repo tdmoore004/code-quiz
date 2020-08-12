e377c1b5300b6e331e843033e35cd2e8a93c5322
Tanner Moore <tdmoore004@gmail.com>
2020-08-11 21:57:15 -0600

    Added variables for navbar items.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Set variables
+const codeQuizHead = document.querySelector(".code-quiz-header");
+const highScoreHead = document.querySelector(".high-score-header");
+const timeDisplay = document.querySelector(".time-display");
+
+console.log(codeQuizHead, highScoreHead, timeDisplay);
